a6bf41baf5a5e9a06df1f505e3b106836f5a963d
Amir Razmjou <arazmj@gmail.com>
2014-09-02 17:23:18 -0400

    Cite Track
    Tracks CiteSeer tweets to build a network.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading;
+
+namespace CiteTrack
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine("Hello World!!! " + DateTime.UtcNow);
+			Thread.Sleep(1000);
+		}
+	}
+}
